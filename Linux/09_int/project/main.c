e804e37473fb1450281427485565d51727646b02
LiNing <1204259355@qq.com>
2025-07-29 20:50:05 +0800

    EXIT

---
+++ Linux/09_int/project/main.c
@@ -0,0 +1,34 @@
+#include "bsp_clk.h"
+#include "bsp_delay.h"
+#include "bsp_led.h"
+#include "bsp_beep.h"
+#include "bsp_key.h"
+#include "bsp_int.h"
+#include "bsp_exit.h"
+
+/*
+ * @description	: main函数
+ * @param 		: 无
+ * @return 		: 无
+ */
+int main(void)
+{
+	unsigned char state = OFF;
+
+	int_init(); 		/* 初始化中断(一定要最先调用！) */
+	imx6u_clkinit();	/* 初始化系统时钟 			*/
+	clk_enable();		/* 使能所有的时钟 			*/
+	led_init();			/* 初始化led 			*/
+	beep_init();		/* 初始化beep	 		*/
+	key_init();			/* 初始化key 			*/
+	exit_init();		/* 初始化按键中断			*/
+
+	while(1)			
+	{	
+		state = !state;
+		led_switch(LED0, state);
+		delay(500);
+	}
+
+	return 0;
+}
